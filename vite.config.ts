8c59537cafc6911ec2598d778e1a1354685e11d4
v1c3nt <vinayakgeorgeciobanu@gmail.com>
2026-08-10 11:15:47 +0200

    build(config): fix import error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import { crx } from '@crxjs/vite-plugin'
 import vue from '@vitejs/plugin-vue'
-import manifest from './src/manifest'
+import manifest from './src/manifest.ts'
 
 export default defineConfig(({ mode }) => {
   return {
